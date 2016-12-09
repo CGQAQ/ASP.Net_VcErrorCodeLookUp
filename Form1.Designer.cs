c0ff025645727a37e346e0e98529f84e485f5d0e
CG <m.jason.liu@outlook.com>
2016-12-10 06:15:10 +0800

    origin

---
+++ Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace VC__6._0ERRLookup
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbxInput = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.lblTip = new System.Windows.Forms.Label();
+            this.lbxResultList = new System.Windows.Forms.ListBox();
+            this.tbxInfo = new System.Windows.Forms.TextBox();
+            this.lleLink = new System.Windows.Forms.LinkLabel();
+            this.lblLink = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbxInput
+            // 
+            this.tbxInput.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.tbxInput.Location = new System.Drawing.Point(12, 32);
+            this.tbxInput.MaxLength = 5;
+            this.tbxInput.Name = "tbxInput";
+            this.tbxInput.Size = new System.Drawing.Size(150, 21);
+            this.tbxInput.TabIndex = 0;
+            this.tbxInput.Tag = "请输入VC++6.0中的错误码";
+            this.tbxInput.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(168, 30);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 1;
+            this.btnSearch.Text = "查询";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // lblTip
+            // 
+            this.lblTip.AutoSize = true;
+            this.lblTip.Location = new System.Drawing.Point(19, 9);
+            this.lblTip.Name = "lblTip";
+            this.lblTip.Size = new System.Drawing.Size(143, 12);
+            this.lblTip.TabIndex = 2;
+            this.lblTip.Text = "请输入VC++6.0中的错误码";
+            this.lblTip.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbxResultList
+            // 
+            this.lbxResultList.FormattingEnabled = true;
+            this.lbxResultList.ItemHeight = 12;
+            this.lbxResultList.Location = new System.Drawing.Point(12, 73);
+            this.lbxResultList.Name = "lbxResultList";
+            this.lbxResultList.Size = new System.Drawing.Size(231, 268);
+            this.lbxResultList.TabIndex = 4;
+            this.lbxResultList.Visible = false;
+            this.lbxResultList.DoubleClick += new System.EventHandler(this.lbxResultList_DoubleClick);
+            // 
+            // tbxInfo
+            // 
+            this.tbxInfo.Location = new System.Drawing.Point(287, 73);
+            this.tbxInfo.Multiline = true;
+            this.tbxInfo.Name = "tbxInfo";
+            this.tbxInfo.Size = new System.Drawing.Size(285, 234);
+            this.tbxInfo.TabIndex = 5;
+            // 
+            // lleLink
+            // 
+            this.lleLink.Location = new System.Drawing.Point(366, 310);
+            this.lleLink.MaximumSize = new System.Drawing.Size(287, 31);
+            this.lleLink.Name = "lleLink";
+            this.lleLink.Size = new System.Drawing.Size(206, 31);
+            this.lleLink.TabIndex = 6;
+            this.lleLink.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lleLink.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lleLink_LinkClicked);
+            // 
+            // lblLink
+            // 
+            this.lblLink.AutoSize = true;
+            this.lblLink.Location = new System.Drawing.Point(287, 310);
+            this.lblLink.Name = "lblLink";
+            this.lblLink.Size = new System.Drawing.Size(77, 12);
+            this.lblLink.TabIndex = 7;
+            this.lblLink.Text = "详情请访问：";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 362);
+            this.Controls.Add(this.lblLink);
+            this.Controls.Add(this.lleLink);
+            this.Controls.Add(this.tbxInfo);
+            this.Controls.Add(this.lbxResultList);
+            this.Controls.Add(this.lblTip);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.tbxInput);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "VC++6.0 error lookup tool";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbxInput;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label lblTip;
+        private System.Windows.Forms.ListBox lbxResultList;
+        private System.Windows.Forms.TextBox tbxInfo;
+        private System.Windows.Forms.LinkLabel lleLink;
+        private System.Windows.Forms.Label lblLink;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Net;
+using System.IO;
+
+namespace VC__6._0ERRLookup
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.MaximizeBox = false;
+            this.TopMost = true;
+            this.FormBorderStyle = FormBorderStyle.Fixed3D;
+            //this.tbxResponce.ScrollBars = ScrollBars.Vertical;
+        }
+
+        private string InputString {
+            get;
+            set;
+        }
+        private string OriginalString {
+            get;
+            set;
+        }
+
+        private string FinalString {
+            get;
+            set;
+        }
+        private string RangeURlString {
+            get;
+            set;
+        }
+
+        private MatchCollection McURL {
+            get;
+            set;
+        }
+        private async void btnSearch_Click(object sender, EventArgs e)
+        {
+            InputString = tbxInput.Text;
+            Regex r = new Regex("C[0-9]{4}");
+            if (!r.IsMatch(InputString))
+            {
+                MessageBox.Show("错误码格式应为C加四位数字！", "输入错误");
+                return;
+            }
+            await SearchAsync();
+
+        }
+        private async Task SearchAsync()
+        {
+            await Task.Run(() =>
+            {
+                try
+                {
+                    //http://services.social.microsoft.com/searchapi/zh-CN/Msdn?query=C2086
+                    HttpWebRequest hw = HttpWebRequest.CreateHttp($"http://services.social.microsoft.com/searchapi/zh-CN/Msdn?query={InputString}") as HttpWebRequest;
+                    hw.Method = "Get";
+                    Stream s = hw.GetResponse().GetResponseStream() as Stream;
+                    StreamReader sr = new StreamReader(s);
+                    OriginalString = sr.ReadToEnd() as string;
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("本工具需要联网使用！");
+                }
+
+            });
+
+            Regex rOriginalList = new Regex("(?<=description.*\"url\":\").*?(?=\",\"display_url)");
+            Match m = rOriginalList.Match(OriginalString);
+            RangeURlString = m.ToString();
+            lleLink.Text = RangeURlString;
+            await GetList();
+            await GetText();
+            return;
+        }
+        private async Task GetText()
+        {
+            try
+            {
+                await Task.Run(() =>
+                {
+                    HttpWebRequest hw = HttpWebRequest.CreateHttp(RangeURlString) as HttpWebRequest;
+                    hw.Method = "Get";
+                    Stream s = hw.GetResponse().GetResponseStream() as Stream;
+                    StreamReader sr = new StreamReader(s);
+                    OriginalString = sr.ReadToEnd() as string;
+                });
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            Regex r = new Regex("(?<=<sentencetext xmlns=\"http://www.w3.org/1999/xhtml\">).*?(?=<)", RegexOptions.IgnoreCase);
+            MatchCollection mc = r.Matches(OriginalString);
+            if (FinalString != null && FinalString.Length > 0)
+            {
+                FinalString = "";
+            }
+            foreach (var item in mc)
+            {
+                FinalString += (item + "\r\n");
+            }
+            tbxInfo.Clear();
+            tbxInfo.Text = FinalString;
+        }
+        private async Task GetList()
+        {
+            await Task.Run(() =>
+            {
+                HttpWebRequest hw = HttpWebRequest.CreateHttp(RangeURlString) as HttpWebRequest;
+                hw.Method = "Get";
+                Stream s = hw.GetResponse().GetResponseStream() as Stream;
+                StreamReader sr = new StreamReader(s);
+                OriginalString = sr.ReadToEnd() as string;
+            });
+            Regex r = new Regex("(?<=toclevel \" data-toclevel=\"1\">\n<a href=\").*?(?=\")", RegexOptions.IgnoreCase);
+            McURL = r.Matches(OriginalString);
+            r = new Regex("(?<=data-toclevel=\"1\">).*?(?=</a>)", RegexOptions.Singleline);//(?<=data-toclevel="1">).*?(?=</a>)
+            MatchCollection mcTitle = r.Matches(OriginalString);
+            if (lbxResultList.Items.Count > 0)
+            {
+                lbxResultList.Items.Clear();
+            }
+            foreach (var item in mcTitle)
+            {
+                lbxResultList.Items.Add(item.ToString().Split('>')[1]);
+            }
+            lbxResultList.Visible = true;
+        }
+
+        private async void lbxResultList_DoubleClick(object sender, EventArgs e)
+        {
+            RangeURlString = McURL[lbxResultList.SelectedIndex].ToString();
+            tbxInput.Clear();
+            tbxInput.Text = lbxResultList.SelectedItem.ToString();
+            await GetText();
+            
+            lleLink.Text = McURL[lbxResultList.SelectedIndex < lbxResultList.Items.Count ? lbxResultList.SelectedIndex : 0].ToString();
+        }
+
+        private void lleLink_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(lleLink.Text);
+        }
+    }
+}
